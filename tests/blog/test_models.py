d3d0e40dfdc7d53fa48e2a350833d399b10b2f14
Edchel Stephen <edchelstephens@gmail.com>
2022-03-15 10:24:51 +0800

    add initial model tests.

--- tests/blog/test_models.py
+++ tests/blog/test_models.py
@@ -1,5 +1,9 @@
-from django.tests import TestCase
+from django.utils import timezone
 
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from blog.models import Post
 
 class PostModelTestCase(TestCase):
     """Post model test case."""
@@ -11,4 +15,36 @@ class PostModelTestCase(TestCase):
 
     def setUp(self) -> None:
         """Run this set up before each test."""
+        self.user = User.objects.create(username="testuser")
+        self.unpublished_post = Post.objects.create(
+            author = self.user,
+            title="Test unpublished post",
+            text="Test",
+            published_date=None
+        )
+        self.published_post = Post.objects.create(
+            author = self.user,
+            title="Test unpublished post",
+            text="Test",
+            published_date=timezone.now()
+        )
+
+    def test_is_published_on_unpublished_post(self) -> None:
+        """Test is_published method on unpublished post."""
+        self.assertFalse(self.unpublished_post.is_published())
+
+    def test_is_published_on_published_post(self) -> None:
+        """Test is_published on published post."""
+        self.assertTrue(self.published_post.is_published())
+
+    def test_publish_method(self) -> None:
+        """Test publish method."""
         pass
+
+    def test_str_method(self) -> None:
+        """Test __str__ method."""
+        post = self.published_post
+        expected = post.title
+
+        self.assertEqual(post.__str__(), expected)
+        self.assertEqual(str(post), expected)
